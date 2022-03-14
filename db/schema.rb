6e7e33e2646040d98600b336b9eb8cec875cc3cb
Senén Rodero Rodríguez <senenrodero@gmail.com>
2022-03-14 13:46:47 +0100

    Add a way to create guest users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_221037) do
+ActiveRecord::Schema.define(version: 2022_03_21_231038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -1625,6 +1625,7 @@ ActiveRecord::Schema.define(version: 2022_03_21_221037) do
     t.boolean "public_interests", default: false
     t.boolean "recommended_debates", default: true
     t.boolean "recommended_proposals", default: true
+    t.boolean "guest", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["date_of_birth"], name: "index_users_on_date_of_birth"
     t.index ["email"], name: "index_users_on_email", unique: true
