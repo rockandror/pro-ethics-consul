1a25e3f999b9bebdd218750d8ac90ee7e9bc7aad
taitus <sebastia.roig@gmail.com>
2022-04-04 15:35:05 +0200

    Enable desired languages

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,5 +1,7 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.available_locales = %w[ar en es fr it nl]
+
     config.autoload_paths << "#{Rails.root}/app/controllers/custom/concerns"
   end
 end
